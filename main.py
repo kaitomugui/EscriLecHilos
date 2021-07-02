e1c2a6d7ca2c798c92645072e3252459eb0c62ae
Franco Ortega <caco.ortega@hotmail.com>
2021-07-02 15:19:46 -0400

    carga tarea anterior

--- main.py
+++ main.py
@@ -1,67 +1,56 @@
-from threading import Thread, Lock, Semaphore
+from threading import Thread, Semaphore
 from time import sleep
-from random import randint
 import logging
 
-mutex = Lock()
+mutex = Semaphore(1)
 bd = Semaphore(1)
-lectores = 0
+cl = 0
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] (%(threadName)-s) %(message)s')
 
-
 def leerBaseDatos():
-    sleep(randint(3, 6))
+    logging.info("Leer Base de Datos")
+    sleep(3)
+
+def usarBasedeDatos():
+    logging.info("Usar Lectura de Datos")
+    sleep(4)
+
+def escribirBaseDatos():
+    logging.info("Escribir Base de Datos")
+    sleep(10)
+
+def pensarDatos():
+    logging.info("(Espera)Pensar que escribir")
+    sleep(6)
 
 
-def escribirBaseDatos(nEscritor):
-    sleep(randint(5, 10))
-
-
-#######
 def lector():
-    global lectores
-
-    while (True):
-        logging.info("Lectura")
-        ####Para Incrementar los Lectores y apoderarse de la base de datos...
+    global cl
+    while(True):
         mutex.acquire()
-        lectores += 1
-
-        if lectores == 1:
+        cl = cl + 1
+        if cl == 1:
             bd.acquire()
-
         mutex.release()
-
-        ####
         leerBaseDatos()
-        ####
-
-        ##### Decrementar los Lectores y librerar la base de datos
         mutex.acquire()
-        lectores -= 1
-
-        if lectores == 0:
+        cl = cl - 1
+        if cl==0:
             bd.release()
-            sleep(randint(2, 5))
-
         mutex.release()
-        sleep(randint(2, 5))
+        usarBasedeDatos()
 
+def escritor():
+    while(True):
+      pensarDatos()
+      bd.acquire()
+      escribirBaseDatos()
+      bd.release()
 
-def escritor(nEscritor):
-    while (True):
-        logging.info("Escribir")
-        bd.acquire()
-        escribirBaseDatos(nEscritor)
-        bd.release()
-
-        sleep(randint(2, 5))
-
-
-escritor1 = Thread(target=escritor, args=(1,))
-escritor2 = Thread(target=escritor, args=(2,))
-escritor3 = Thread(target=escritor, args=(3,))
+escritor1 = Thread(target=escritor)
+escritor2 = Thread(target=escritor)
+escritor3 = Thread(target=escritor)
 
 lector1 = Thread(target=lector)
 lector2 = Thread(target=lector)
@@ -75,4 +64,3 @@ escritor1.start()
 escritor2.start()
 escritor3.start()
 
-escritor1.join()
